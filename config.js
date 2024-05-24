fc62690a33821f2c16ead6cbae7d15725a2b52ed
imlmonterrrey <imlmonterrrey@gmail.com>
2024-05-24 11:50:02 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwU_hXyrK_Ivto7TzdF3GpPoQ3pX3f7sgezGiyNzY_ORHJs0kY4cqn6u2BINXabDKxJsA/exec";
